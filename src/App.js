b9408f05ae47924e20c7843e55d6e113d0b3418b
aclementi26 <67277904+aclementi26@users.noreply.github.com>
2020-09-22 15:21:43 -0400

    set Default city

--- src/App.js
+++ src/App.js
@@ -2,33 +2,23 @@ import React from "react";
 import "./App.css";
 import robot from "./robot2.svg";
 import Search from "./Search";
-import CityInfo from "./cityInfo";
-import "./cityInfo.css";
-import WeatherInfo from "./weatherInfo";
-import "./weatherInfo.css";
-import Forecast from "./forecast";
-import "./forecast.css";
+
 export default function App() {
   return (
     <div className="App">
       <div className="box">
-        <div className="time">Last Update: Wednesday</div>
         <div className="glass"></div>
         <div className="content">
           <h1>Weather Bot</h1>
           <div>
-            <img src={robot} alt="this is robot" class="pic" />
+            <img src={robot} alt="this is robot" className="pic" />
           </div>
 
           <div className="info"></div>
 
-          <Search />
+          <Search defaultCity="Philadelphia" />
 
-          <CityInfo />
-
-          <WeatherInfo />
-          <Forecast />
-          <p class="website">
+          <p className="website">
             <a
               href="https://github.com/aclementi26/weatherapp-react"
               rel="noopener noreferrer"
