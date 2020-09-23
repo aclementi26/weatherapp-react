1bd21147efae5217f9d3742525503e8bd1879898
aclementi26 <67277904+aclementi26@users.noreply.github.com>
2020-09-23 13:42:59 -0400

    Update weatherIcon.js

--- src/weatherIcon.js
+++ src/weatherIcon.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
-export default function weatherIcon() {
+export default function weatherIcon(props) {
+  const codeMappingInfo = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "SLEET",
+    "09n": "SLEET",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "WIND",
+    "11n": "WIND",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
+  };
   return (
     <ReactAnimatedWeather
-      icon="CLEAR_DAY"
+      icon={codeMappingInfo[props.code]}
       color="Yellow"
       size="60"
       animate="true"
