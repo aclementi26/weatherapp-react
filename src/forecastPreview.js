b60cd4f81d4ca8b676f4c04f469529945418ee77
aclementi26 <67277904+aclementi26@users.noreply.github.com>
2020-09-24 16:18:49 -0400

    Create forecastPreview.js

---
+++ src/forecastPreview.js
@@ -0,0 +1,24 @@
+import React from "react";
+import WeatherIcon from "./weatherIcon";
+
+export default function ForecastPreview(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000);
+    let hours = date.getHours();
+    return `${hours}:00`;
+  }
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-2">{hours()}</div>
+        <div className="col-2">
+          {" "}
+          <div className="icon">
+            <WeatherIcon code={props.data.weather[0].icon} />
+          </div>
+        </div>
+        <div className="col-2">{Math.round(props.data.main.temp)}</div>
+      </div>
+    </div>
+  );
+}
