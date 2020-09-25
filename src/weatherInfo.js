7dd28e80af7f7f2e4696c97bc89c542d7f5d4b64
aclementi26 <67277904+aclementi26@users.noreply.github.com>
2020-09-25 13:56:49 -0400

    Update weatherInfo.js

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -5,14 +5,14 @@ export default function WeatherInfo(props) {
     <div className="city">
       <div className="col-6">
         <ul>
-          <li>
+          <li className="text-capitalize">
             <span id="sky"></span> {props.weather.description}
           </li>
-          <li>
+          <li className="text-capitalize">
             <span id="humidity"></span>humidity:{props.weather.humidity}
             {""}%
           </li>
-          <li>
+          <li className="text-capitalize">
             <span id="wind"> </span>wind:{Math.round(props.weather.wind)}
             {""}mph
           </li>
