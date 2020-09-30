9034444dce1ab8efdd2baed9ba0d3f6a5469481a
aclementi26 <67277904+aclementi26@users.noreply.github.com>
2020-09-30 11:18:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
               rel="noopener noreferrer"
             >
               Open-source code
-            </a>
+            </a>{" "}
             by Alison Clementi
           </p>
         </div>
